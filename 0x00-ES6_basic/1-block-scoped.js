0679f98df80fc43367002bf057be37838f7bfb97
Daniel kamau <danielkamaunganga3@gmail.com>
2024-09-30 16:42:42 +0300

    Fix code error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
-export default function taskBlock (trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-	// eslint-disable-next-line no-empty
+  // eslint-disable-next-line no-empty
   if (trueOrFalse) {
-	  
   }
 
   return [task, task2];
