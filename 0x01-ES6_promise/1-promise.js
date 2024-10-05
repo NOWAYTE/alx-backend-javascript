750f76901efff582a06857e4d83c3782034536c9
Daniel kamau <danielkamaunganga3@gmail.com>
2024-10-05 16:34:39 +0300

    Update

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,6 @@
-export function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success == true) {
-            resolve({ status: 200, body: "Success" });
-        } else {
-            reject("The fake API is not working");
-        }
-    });
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(Error('The fake API is not working currently'));
+  });
 }
-
