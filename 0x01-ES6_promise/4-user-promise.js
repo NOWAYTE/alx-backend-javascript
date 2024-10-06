046a4b95be7cad55378fb7d137bab1552fe4071e
Daniel kamau <danielkamaunganga3@gmail.com>
2024-10-06 12:04:31 +0300

    Fix eslint error

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,6 @@
-export function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-        firstName,
-        lastName
-    });
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
 }
-
