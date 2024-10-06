a2af6be49e4e12ea09f6f77a44b64e3dd9a5f784
Daniel kamau <danielkamaunganga3@gmail.com>
2024-10-06 11:17:50 +0300

    Update

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,3 @@
-export function handleResponseFromAPI(promise) {
-    return promise
-        .then(() => {
-            return { status: 200, body: "success" };
-        })
-        .catch(() => {
-            return new Error();
-        })
-        .finally(() => {
-            console.log("Got a response from the API");
-        });
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error()).finally(() => { console.log('Got a response from the API'); });
 }
