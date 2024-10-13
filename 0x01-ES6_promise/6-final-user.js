1afce58172ad8672273266043881f45e255b007c
Daniel kamau <danielkamaunganga3@gmail.com>
2024-10-13 14:35:59 +0300

    Fix typo

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,20 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignUp(firstName, lastName, filename){
-	const res = [];
+export default async function handleProfileSignUp(firstName, lastName, fileName) {
+  const res = [];
 
-	try {
-		const user = await signUpUser(firstName, lastName);
-		res.push( { status: 'fulfilled', value: user } );
-		await uploadPhoto(fileName);
-		
-	} catch (error) {
-		res.push( {
-			status: 'rejected',
-			value: 'Error:  cannot  be processed',
-		});
-
-	}
-	return res;
-
+  try {
+    const user = await signUpUser(firstName, lastName);
+    res.push({
+      status: 'fulfilled', value: user,
+    });
+    await uploadPhoto(fileName);
+  } catch (error) {
+    res.push({
+      status: 'rejected',
+      value: 'Error:  cannot  be processed',
+    });
+  }
+  return res;
 }
