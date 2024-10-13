4401130527337740afe6f2d2fc85642c5777bff0
Daniel kamau <danielkamaunganga3@gmail.com>
2024-10-13 14:38:49 +0300

    Fix error logging

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,13 +7,14 @@ export default async function handleProfileSignUp(firstName, lastName, fileName)
   try {
     const user = await signUpUser(firstName, lastName);
     res.push({
-      status: 'fulfilled', value: user,
+      status: 'fulfilled', 
+		value: user,
     });
     await uploadPhoto(fileName);
   } catch (error) {
     res.push({
       status: 'rejected',
-      value: 'Error:  cannot  be processed',
+      value: `Error: ${fileName} cannot be processed`,
     });
   }
   return res;
