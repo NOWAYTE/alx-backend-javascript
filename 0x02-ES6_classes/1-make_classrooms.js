fec1c0523940772a80d5e619bd39efbf668dd5c3
Daniel kamau <danielkamaunganga3@gmail.com>
2024-10-21 01:33:26 +0300

    Code update

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,5 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const array = [19, 20, 34];
-
-  array.forEach((i) => {
-    const classRoom = new ClassRoom(i);
-    console.log(classRoom);
-  });
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
