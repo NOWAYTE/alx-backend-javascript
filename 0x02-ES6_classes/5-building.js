4a6719dbad578c1cabac4253d419fb5c14c1064a
Daniel kamau <danielkamaunganga3@gmail.com>
2024-10-21 19:19:37 +0300

    Update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,20 @@
-export default abstract class Building {
-	constructor(sqft) {
-		if (typeof sqft !== 'number') {
-			throw new error();
-		}
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new Error();
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
 
-		this._sqft = sqft;
+  get sqft() {
+    return this._sqft;
+  }
 
-		get sqft() {
-			return this._sqft;
-		}
+  set sqft(value) {
+    this._sqft = value;
+  }
 
-		set sqft(newSqft) {
-			this._sqft = newSqft;
-		}
-
-		abstract evacuationWarningMessage() {
-			throw new Error('Class extending Building must override evacuationWaringMessage');
-		}
-	}
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
-
