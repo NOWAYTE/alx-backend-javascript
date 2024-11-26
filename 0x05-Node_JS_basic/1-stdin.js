dede43cb201e3d88afa3443e4be9864aae18f6a0
Daniel kamau <danielkamaunganga3@gmail.com>
2024-11-27 02:13:36 +0300

    Update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holbeton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-  console.log(`Your name is ${data.toString().trim()}`);
+  console.log(`Your name is: ${data.toString().trim()}`);
   process.stdout.write('This important software is now closing\n');
 
   process.stdin.pause();
