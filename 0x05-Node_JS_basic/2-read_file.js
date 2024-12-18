3121567d351aa67d87f8ee015be163e4c0513f04
Daniel kamau <danielkamaunganga3@gmail.com>
2024-12-18 23:56:38 +0300

    Update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,17 +1,24 @@
-const fs = require('fs')
+const fs = require('fs');
 
 function countStudents(path) {
-	try{
-		const data = fs.readFileSync(path, 'utf-8');
-		const lines = data.trim().split('\n');
-		console.log(lines)
-		const header = lines[0].split(',');
-		console.log(header)
-		const students = lines.slice(1)
-		console.log(`Number of students: ${students.length}`);
-	} catch (err) {
-		process.stdout.write("Cannot load the database\n")
-	}
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const rows = data.trim().split(',');
+    const headers = rows.shift().split(',');
+    const students = rows
+      .map((row) => row.split(','))
+      .filter((row) => row.length === headers.length)
+      .map((row) => ({
+        firstname: row[0].trim(),
+        lastname: row[1].trim(),
+        age: row[2].trim(),
+        field: row[3].trim(),
+      }));
 
+	  console.log(students)
+  } catch (error) {
+    console.log('Cannot load the database');
+  }
 }
+
 module.exports = countStudents;
