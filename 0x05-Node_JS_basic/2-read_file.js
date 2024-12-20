148fa85d8545b5ad16062241eb20681a054febd8
Daniel kamau <danielkamaunganga3@gmail.com>
2024-12-20 18:33:12 +0300

    Read file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,24 +1,21 @@
 const fs = require('fs');
-const path = require('path');
 
-function countStudents(pathToFile) {
+function countStudents(path) {
   try {
-    const data = fs.readFileSync(pathToFile, 'utf8');
+    const data = fs.readFileSync(path, 'utf8');
 
     const rows = data.trim().split('\n');
 
     const headers = rows.shift().split(',');
     const students = rows
-      .map(row => row.split(','))
-      .filter(row => row.length === headers.length)
-      .map((row) => {
-        return {
-          firstname: row[0].trim(),
-          lastname: row[1].trim(),
-          age: row[2].trim(),
-          field: row[3].trim(),
-        };
-      });
+      .map((row) => row.split(','))
+      .filter((row) => row.length === headers.length)
+      .map((row) => ({
+        firstname: row[0].trim(),
+        lastname: row[1].trim(),
+        age: row[2].trim(),
+        field: row[3].trim(),
+      }));
 
     console.log(`Number of students: ${students.length}`);
 
@@ -33,10 +30,9 @@ function countStudents(pathToFile) {
     for (const [field, studentsInField] of Object.entries(fieldCounts)) {
       console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.join(', ')}`);
     }
-
   } catch (err) {
     console.error('Cannot load the database');
   }
 }
 
-module.exports = countStudents
+module.exports = countStudents;
