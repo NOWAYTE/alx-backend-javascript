6af3991f9c946d57cd342cdb16e7eae3d1a9d90d
Daniel kamau <danielkamaunganga3@gmail.com>
2024-12-21 13:48:10 +0300

    Read a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,40 +1,39 @@
 const fs = require('fs').promises;
 
 async function countStudents(path) {
-    try {
-        const data = await fs.readFile(path, "utf-8");
-        const rows = data.trim().split('\n');
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const rows = data.trim().split('\n');
 
-        const headers = rows.shift().split(',');
+    // const headers = rows.shift().split(',');
 
-        const students = rows.map((row) => row.split(',')).map((rows) => ({
-            firstname: rows[0].trim(),
-            lastname: rows[1].trim(),
-            age: rows[2].trim(),
-            field: rows[3].trim(),
-        }));
+    const students = rows.map((row) => row.split(',')).map((rows) => ({
+      firstname: rows[0].trim(),
+      lastname: rows[1].trim(),
+      age: rows[2].trim(),
+      field: rows[3].trim(),
+    }));
 
-        console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students: ${students.length}`);
 
-        const fieldCount = {};
+    const fieldCount = {};
 
-        students.forEach((student) => {
-            if (!fieldCount[student.field]) {
-                fieldCount[student.field] = [];
-            }
-            fieldCount[student.field].push(student.firstname);
-        });
+    students.forEach((student) => {
+      if (!fieldCount[student.field]) {
+        fieldCount[student.field] = [];
+      }
+      fieldCount[student.field].push(student.firstname);
+    });
 
-        Object.keys(fieldCount).forEach((field) => {
-            const studentField = fieldCount[field];
-            console.log(
-                `Number of students in ${field}: ${studentField.length}. List: ${studentField.join(', ')}`
-            );
-        });
-    } catch (error) {
-        console.log(`Error reading file: ${error.message}`);
-    }
+    Object.keys(fieldCount).forEach((field) => {
+      const studentField = fieldCount[field];
+      console.log(
+        `Number of students in ${field}: ${studentField.length}. List: ${studentField.join(', ')}`,
+      );
+    });
+  } catch (error) {
+    console.log(`Error reading file: ${error.message}`);
+  }
 }
 
 module.exports = countStudents;
-
