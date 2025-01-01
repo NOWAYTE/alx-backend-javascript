275e2a7543a056539b5bc976513679521aa32b53
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-02 02:22:02 +0300

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString){
+	let res = ''
+	if (!startString) return res
+	set.forEach((value) => {
+		if(value.startsWith(startString)){
+			res +=  `${value.slice(startString.length)}-`;
+		}
+	})
+
+	return res.slice(0, res.length, -1)
+}
