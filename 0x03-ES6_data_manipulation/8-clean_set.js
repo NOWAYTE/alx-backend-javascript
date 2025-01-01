adea866e06a5e708bd9236d47de43ee3bd004639
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-02 02:26:18 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
-export default function cleanSet(set, startString){
-	let res = ''
-	if (!startString) return res
-	set.forEach((value) => {
-		if(value.startsWith(startString)){
-			res +=  `${value.slice(startString.length)}-`;
-		}
-	})
+export default function cleanSet(set, startString) {
+  let res = '';
+  if (!startString || !startString) return res;
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      res += `${value.slice(startString.length)}-`;
+    }
+  });
 
-	return res.slice(0, res.length, -1)
+  return res.slice(0, res.length, -1);
 }
