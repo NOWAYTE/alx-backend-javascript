af0722a68f3edc794a0f79f4a00f75aa07d68b0e
Daniel kamau <danielkamaunganga3@gmail.com>
2025-01-02 03:26:07 +0300

    Update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
+// eslint-disable-next-line consistent-return
 export default function cleanSet(set, startString) {
   let res = '';
-  if (!startString || !startString) return res;
-  set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      res += `${value.slice(startString.length)}-`;
+  if (!startString || !startString.length) return res;
+  for (const el of set) {
+    if (el && el.startsWith(startString)) {
+      res += `${el.slice(startString.length)}-`;
     }
-  });
-
-  return res.slice(0, res.length -1);
+  }
+  return res.slice(0, res.length - 1);
 }
